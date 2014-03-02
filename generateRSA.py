131562d1a3af6705d67667c2abd3b3ceaf73b305
epimetheusincarnate <dvalderrama08@gmail.com>
2014-03-01 22:54:32 -0800

    Run this to create public/private keys written out to 2 files

---
+++ generateRSA.py
@@ -0,0 +1,20 @@
+from Crypto.PublicKey import RSA
+def generate_RSA(bits=2048):
+  '''
+  Generate an RSA keypair with an exponent of 65537 in PEM format
+  param: bits The key length in bits
+  Return private key and public key
+  '''
+  new_key = RSA.generate(bits, e=65537)
+  public_key = new_key.publickey().exportKey("PEM")
+  private_key = new_key.exportKey("PEM")
+  return private_key, public_key
+
+if __name__ == "__main__":
+
+  private_key, public_key = generate_RSA()
+
+  with open('private_key.txt', 'w') as file:
+    file.write(private_key)
+  with open('public_key.txt', 'w') as file:
+    file.write(public_key)
